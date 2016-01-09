8d3f22aa2a2b2500023c696af603d220ee52a8a2
Uladzimir Vysotski <Uladzimir.Vysotski@easyjet.com>
2016-01-09 15:51:45 +0300

    Initial commit 	new file:   Mentoring.IQueryable.Expressions.sln 	new file:   Mentoring.IQueryable.Expressions/App.config 	new file:   Mentoring.IQueryable.Expressions/Mentoring.IQueryable.Expressions.csproj 	new file:   Mentoring.IQueryable.Expressions/Program.cs 	new file:   Mentoring.IQueryable.Expressions/Properties/AssemblyInfo.cs 	new file:   Mentoring.IQueryable.Expressions/TransformingExpressionVisitor.cs 	new file:   Mentoring.IQueryable.Expressions/bin/Debug/Mentoring.IQueryable.Expressions.exe.config 	new file:   Mentoring.IQueryable.Expressions/bin/Debug/Mentoring.IQueryable.Expressions.vshost.exe.config 	new file:   Mentoring.IQueryable.Expressions/obj/Debug/Mentoring.IQueryable.Expressions.csproj.FileListAbsolute.txt 	new file:   Mentoring.IQueryable.Expressions/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   Mentoring.IQueryable.Expressions/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   Mentoring.IQueryable.Expressions/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs

---
+++ Mentoring.IQueryable.Expressions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Mentoring.IQueryable.Expressions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<int, int>> expr = a => a + 1;
+            Expression<Func<int, int>> expr2 = a => a - 1;
+            Console.WriteLine(expr);
+
+            var treeModifierExpressionVisitor = new TransformingExpressionVisitor();
+            var modifiedExpr = treeModifierExpressionVisitor.Modify(expr);
+            var modifiedExpr2 = treeModifierExpressionVisitor.Modify(expr2);
+
+            Console.WriteLine(modifiedExpr);
+            Console.WriteLine(modifiedExpr2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Mentoring.IQueryable.Expressions/TransformingExpressionVisitor.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Mentoring.IQueryable.Expressions
+{
+    public class TransformingExpressionVisitor : ExpressionVisitor
+    {
+        private Dictionary<ParameterExpression, ConstantExpression> _parameterValues;
+
+        public void SetParameterValues(Dictionary<ParameterExpression, ConstantExpression> parameterValues)
+        {
+            this._parameterValues = parameterValues;
+        }
+
+        public Expression Modify(Expression expression)
+        {
+            return this.Visit(expression);
+        }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            var left = this.Visit(node.Left);
+            var right = this.Visit(node.Right);
+
+            if (right.NodeType == ExpressionType.Constant && left.NodeType == ExpressionType.Parameter && (node.NodeType == ExpressionType.Add || node.NodeType == ExpressionType.Subtract))
+            {
+                var value = this.EvaluateExpression(right);
+                if (value.GetValueOrDefault() == 1)
+                {
+                    var operation = node.NodeType == ExpressionType.Add
+                                       ? ExpressionType.Increment
+                                       : ExpressionType.Decrement;
+
+                    return Expression.MakeUnary(operation, left, typeof(int));
+                }
+
+            }
+
+            return base.VisitBinary(node);
+        }
+
+        protected int? EvaluateExpression(Expression expression)
+        {
+            var lambda = Expression.Lambda(expression);
+
+            var compiled = lambda.Compile();
+
+            var value = compiled.DynamicInvoke(null);
+            return (int?)value;
+        }
+        
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            if (this._parameterValues == null || this._parameterValues.Count == 0)
+            {
+                return base.VisitParameter(node);
+            }
+
+            ConstantExpression replacement;
+
+            if (this._parameterValues.TryGetValue(node, out replacement))
+            {
+                return replacement;
+            }
+
+            return base.VisitParameter(node);
+        }
+    }
+}
